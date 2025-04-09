e1a5e8e50e43aab33ab35948965940654c930e98
murphyry <ryan.p.murphy@dell.com>
2025-04-09 14:42:22 -0400

    formatting and comment cleanup

--- powerflex_host_capacity.sh
+++ powerflex_host_capacity.sh
@@ -1,14 +1,15 @@
 #!/bin/bash 
-# This is a simple bash script that uses curl to make API calls, and jq to parse the json from those calls.
-# Make sure you have curl and jq installed
 
-#SCRIPT VARIABLES - PUT YOUR POWERFLEX MANAGER CREDENTIALS IN HERE
-PFXM_IP='YOUR_IP'
+########################################################################################################################################################### 
+#SCRIPT VARIABLES - SET YOUR POWERFLEX MANAGER CREDENTIALS HERE
+########################################################################################################################################################### 
+PFXM_IP='YOUR_PFXM_IP'
 PFXM_USER='YOUR_USERNAME'
 PFXM_PASSWORD='YOUR_PASSWORD'
 
 ###########################################################################################################################################################  
-#SCRIPT COLORS FOR ECHO  
+
+#SCRIPT COLORS FOR ECHO OUTPUT  
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 CYAN='\033[0;36m'
@@ -16,7 +17,7 @@ LIGHT_PURPLE='\033[1;35m'
 YELLOW='\033[1;33m'  
 NC='\033[0m'
 
-#Start SCRIPT
+#START SCRIPT
 echo " "
 echo -e "${YELLOW}######################################################################################################## ${NC}"
 echo -e "${YELLOW}# PowerFlex 4.6+ Host Capacity Script ${NC}"
@@ -31,46 +32,46 @@ echo " "
 TOKEN=$(curl -s -k --location --request POST "https://${PFXM_IP}/rest/auth/login" --header "Accept: application/json" --header "Content-Type: application/json" --data "{\"username\": \"${PFXM_USER}\",\"password\": \"${PFXM_PASSWORD}\"}") 
 ACCESS_TOKEN=$(echo "${TOKEN}" | jq -r .access_token) 
 
-#Get system id
+#Get the system id to use for the csv file name
 SYSTEM=$(curl -k -s -X GET "https://$PFXM_IP/api/types/System/instances/" -H 'Accept: application/json' -H 'Content-Type: application/json' -H "Authorization: Bearer $ACCESS_TOKEN")
 SYSTEM_ID=$(echo $SYSTEM | jq .[].id| tr -d '"')
 echo -e "${GREEN}[SUCCESS] - Connected to PowerFlex system ${SYSTEM_ID}${NC}"
 echo " "
 echo -e "${CYAN}[QUERYING HOSTS]${NC}"
 
-#Create CSV to hold information for hosts
+#Create CSV file to hold information for hosts
 CSV_NAME="${SYSTEM_ID}_capacity_report.csv"
 echo "HOST_NAME,SDC_ID,OPERATING_SYSTEM,SDC_STATE,SDC_VERSION,VOLUMES_MAPPED,VOLUME_PROVISIONED_KIB,VOLUME_USED_KIB" > $CSV_NAME
 
-#Get all SDCs and then extract all SDC IDs into an array
+#Get all SDCs connected to the system and extract all SDC IDs into an array
 HOSTS=$(curl -k -s -X GET "https://$PFXM_IP/api/types/Sdc/instances" -H 'Accept: application/json' -H 'Content-Type: application/json' -H "Authorization: Bearer $ACCESS_TOKEN")
 HOST_IDS=$(echo $HOSTS | jq .[].id)
 readarray -t bash_array < <(echo "$HOST_IDS")
 
 
-#For each SDC look up its name, volumes, and total up volume allocated
+#For each SDC ID look up its name, volumes, version, state, os, size, total provisioned, and allocated/consumed
 for HOST in "${bash_array[@]}"; do
+  #extract the SDC ID into a format that works with curl
   SDC_ID=$(echo $HOST | tr -d '"')
   
   #Get all mapped volumes for the SDC ID
   VOLUMES=$(curl -k -s -X GET "https://$PFXM_IP/api/instances/Sdc::$SDC_ID/relationships/Volume" -H "Authorization: Bearer $ACCESS_TOKEN" -H "Accept: application/json")
   VOLUME_IDS=$(echo $VOLUMES | jq -r '.[].id')
   
-  #Get the SDC's name
+  #Get the SDC's info
   SDC_INFO=$(curl -k -s -X GET "https://$PFXM_IP/api/instances/Sdc::$SDC_ID" -H "Authorization: Bearer $ACCESS_TOKEN" -H "Accept: application/json")
   SDC_NAME=$(echo $SDC_INFO | jq .name | tr -d '"')
   SDC_OS=$(echo $SDC_INFO | jq .osType | tr -d '"')
   SDC_STATE=$(echo $SDC_INFO | jq .mdmConnectionState | tr -d '"')
   SDC_VERSION=$(echo $SDC_INFO | jq .installedSoftwareVersionInfo | tr -d '"')
-
-  #Progress
   echo -e "${CYAN}-HOST [${SDC_NAME}] FOUND${NC}"   
   
-  #Total up Used for this SDC 
-  TOTAL_IN_USE_KIB=0
+  #Variables to total up the volume stats for this SDC
   TOTAL_VOLUMES=0
   TOTAL_SIZE_KIB=0
-  #for each volume id
+  TOTAL_IN_USE_KIB=0
+  
+  #for each volume id collect its information and add it to the totals
   for volume in $VOLUME_IDS; do
     #get all volume info
     VOLUME_INFO=$(curl -k -s -X GET "https://$PFXM_IP/api/instances/Volume::$volume" -H "Authorization: Bearer $ACCESS_TOKEN" -H "Accept: application/json")
@@ -88,17 +89,18 @@ for HOST in "${bash_array[@]}"; do
     TOTAL_VOLUMES=$(($TOTAL_VOLUMES + 1))
     TOTAL_SIZE_KIB=$(($TOTAL_SIZE_KIB + $VOLUME_SIZE))
   done
-  #Add entry to CSV
+  
+  #Add the SDC's entry to CSV file
   echo "${SDC_NAME},${SDC_ID},${SDC_OS},${SDC_STATE},${SDC_VERSION},${TOTAL_VOLUMES},${TOTAL_SIZE_KIB},${TOTAL_IN_USE_KIB}" >> $CSV_NAME
 
-  #Print out total capacity
   sleep 1
+  
   echo -e "${CYAN}-HOST [$SDC_NAME] COMPLETE ${NC}"  
   
-  #sleep to slow the API calls
   sleep 2
 done
 
+#print out the final status
 echo -e "${CYAN}[QUERYING HOSTS COMPLETE]${NC}"
 echo " "
 echo -e "${GREEN}######################################################################################################## ${NC}"
